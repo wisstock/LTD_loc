f0bf9b1bd1813ce5affa267500a72f19c3aec3d1
wisstock <omnia.fatum@gmail.com>
2021-11-16 17:21:27 +0200

    spine detection done

--- electro_demo.py
+++ electro_demo.py
@@ -28,17 +28,29 @@ logging.basicConfig(level=logging.INFO,
 data_path = os.path.join(sys.path[0], 'data')
 raw_rec = hr.Bundle(f'{data_path}/cell1.dat')
 
-series_num = 0
-series_count =  len(raw_rec.pul[0])
 
-print(raw_rec.pul[0][4])
 
-for series_num in range(0, series_count):
-    logging.info(f'Pulse protocol {raw_rec.pul[0][series_num].Label},  {raw_rec.pul[0][series_num].NumberSweeps} sweeps')
-    series_num += 1
 
+all_series_names = [raw_rec.pul[0][i].Label for i in range(0, len(raw_rec.pul[0]))]
+logging.info(f'Series in protocol:\n{all_series_names}')
+
+# for series_num in range(0, series_count):
+#     logging.info(f'Pulse protocol {raw_rec.pul[0][series_num].Label},  {raw_rec.pul[0][series_num].NumberSweeps} sweeps')
+#     series_num += 1
+
+series_num = 5
+logging.info(f'Selected series metha:\n{raw_rec.pul[0][series_num]}')
+
+series_time = raw_rec.pul[0][series_num].Time
+series_gain = raw_rec.pul[0][series_num].AmplifierState.RealF2Bandwidth
+logging.info(f'Series time: {series_time}, gain: {series_gain}')
+
+time_line = np.arange(len(raw_rec.data[0, series_num, 0, 0])) * 5e-5  # (series_time / 1.0e14)
+print(time_line)
+
+logging.info(f'Swepps of {raw_rec.pul[0][series_num].Label} series')
 fig = plt.figure(figsize=(12, 8))
 ax0 = fig.add_subplot()
-for i in range(0, raw_rec.pul[0][5].NumberSweeps):
-    ax0.plot(raw_rec.data[0, 5, i, 0], alpha=.5)
+for i in range(0, raw_rec.pul[0][series_num].NumberSweeps):
+    ax0.plot(time_line, raw_rec.data[0, series_num, i, 0] * series_gain, alpha=.5)
 plt.show()
--- fluo_demo.py
+++ fluo_demo.py
@@ -44,7 +44,7 @@ if not os.path.exists(res_path):
     os.makedirs(res_path)
 
 
-img_series = tifffile.imread(f'{data_path}/corr.tif')  # DiffHold_435nm_1
+img_series = tifffile.imread(f'{data_path}/DiffHold_435nm_1.tif')  # DiffHold_435nm_1
 print(np.shape(img_series))
 # img_series = img_series[:, 250:370, 80:200]
 
@@ -55,9 +55,9 @@ print(np.shape(img_series))
 img_norm = filters.rank.median(img_series[0], morphology.disk(5)) 
 
 # create mask for nucleus area
-nucleus_mask = img_norm < np.max(img_norm)*0.95  # mask bright nucleus area
+nucleus_mask = img_norm < np.max(img_norm)*0.99  # mask bright nucleus area
 distances, _ = distance_transform_edt(nucleus_mask, return_indices=True)
-expand_nucleus_mask = distances <= 15  # extend nucleus mask
+expand_nucleus_mask = distances <= 20  # extend nucleus mask
 
 # create mask bright areas
 # markers = exposure.adjust_log(img_norm, 1) #  filters.rank.entropy(img_norm, morphology.disk(12))  # 
@@ -66,27 +66,46 @@ th_neuron = filters.threshold_otsu(markers)
 mask = morphology.closing(markers > th_neuron, morphology.square(1))
 
 # get mask for largest element only
-element_label = measure.label(mask)
-element_props = measure.regionprops(element_label)
-element_area = {element.area : element.label  for element in element_props}
-neuron_mask = element_label == element_area[max(element_area.keys())]
+neuron_label = measure.label(mask)
+neuron_props = measure.regionprops(neuron_label)
+neuron_area = {neuron_element.area : neuron_element.label  for neuron_element in neuron_props}
+neuron_mask = neuron_label == neuron_area[max(neuron_area.keys())]
 neuron_mask[expand_nucleus_mask] = 0
 
-# sctive spines detection
+# active regions detection
 der_series = u.series_derivate(img_series,
                                mask=neuron_mask,
                                sigma=2, kernel_size=20,
                                left_w=5, space_w=10, right_w=5)
-der_std = ma.masked_where(~neuron_mask, np.std(der_series, axis=0))  # final derivate SD image
+der_std = np.std(der_series, axis=0)
+# der_std_masked = ma.masked_where(~neuron_mask, der_std)  # final derivate SD image
 th_spine = filters.threshold_yen(der_std)
-spine_mask = der_std > th_spine
+spine_mask = der_std> th_spine
+spine_mask[~neuron_mask] = 0
 
+# potential spine regions filtering
 spine_label, spine_num = measure.label(spine_mask, return_num=True)
-logging.info(f'{spine_num} spine regions finded')
-spine_label = ma.masked_where(~neuron_mask, spine_label)
+logging.info(f'{spine_num} potential spine regions finded')
 
-spine_props = measure.regionprops(spine_label, intensity_image=der_std)
-spine_overlay = label2rgb(spine_label, image=img_norm, bg_label=0, alpha=0.4)
+spine_props = measure.regionprops(spine_label)  # , intensity_image=der_std
+spine_area = {spine.area : spine.label  for spine in spine_props}
+
+spine_min_area = 30  # minimal spime area in px
+s_i = 0
+for sa in spine_area.keys():
+    if sa < spine_min_area:
+        spine_label[spine_label == spine_area[sa]] = 0
+        s_i += 1
+logging.info(f'{s_i} spine regions removed, minimal spine area - {spine_min_area}px')
+spine_label = spine_label > 0
+
+# final spine label mask
+spine_label_fin, spine_num_fin = measure.label(spine_label, return_num=True)
+logging.info(spine_num_fin)
+spine_props_fin = measure.regionprops(spine_label_fin)
+
+spine_label_fin = ma.masked_where(~neuron_mask, spine_label_fin)
+spine_overlay = label2rgb(spine_label_fin, image=img_norm, bg_label=0, alpha=0.4)
 
 # fig, ax = plt.subplots(figsize=(10,10))
 # ax.imshow(spine_label, cmap='jet')
@@ -100,17 +119,17 @@ plt.figure(figsize=(20,10))
 # fig, axes = plt.subplots(nrows=1, ncols=4, figsize=(10,10), sharex=True, sharey=True)
 # ax = axes.ravel()
 ax0 = plt.subplot(231)
-ax0.imshow(img_norm, cmap='magma')
+ax0.imshow(img_norm, cmap='jet')
 ax0.axis('off')
 ax1 = plt.subplot(232)
-ax1.imshow(der_std, cmap='jet')
+ax1.imshow(ma.masked_where(~neuron_mask, der_std), cmap='jet')
 ax1.axis('off')
 ax2 = plt.subplot(233)
-ax2.imshow(spine_overlay)
+ax2.imshow(spine_overlay, cmap='jet')
 ax2.axis('off')
 ax3 = plt.subplot(212)
 
-for region in spine_props:
+for region in spine_props_fin:
     spine_coord = (region.centroid[1], region.centroid[0])  # spine_mask.shape[0] - 
     if spine_coord[0] is None:
         logging.warning('Spine region with incorrect properties!')
@@ -124,7 +143,8 @@ for region in spine_props:
                    xytext=(spine_coord[0], spine_coord[1]-20),
                    arrowprops=dict(width=2, headwidth=5, headlength=10, facecolor='white', shrink=0.01))
 
-    region_mask = spine_label == region.label
+    # individual spine time intensity profile
+    region_mask = spine_label_fin == region.label
     region_mask = region_mask != 0
     spine_series_slice = [np.mean(ma.masked_where(~region_mask, frame)) for frame in img_series]
     ax3.plot(spine_series_slice, label=region.label)
